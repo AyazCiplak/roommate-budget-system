55a72f38cea12135c295822bbef8429f4d3c83d2
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-06-27 23:35:19 -0400

    Initial navbar component added

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>(Title)</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                {
+                //Add new link when page is set up
+                }
+                <a href="/">New Expense</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
