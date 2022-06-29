bc2c6fa2135c8c8f2e1dd3e3926d26b308a8d096
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-06-29 15:27:33 -0400

    Added sample arrays of users and expenses to display

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,13 +1,35 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Expenses = () => {
-    
+    //Array of sample users
+const [expenses, setExpenses] = useState([
+    { title: 'Expense 1', description: 'lorem ipsum', cost: '350', id: 1 },
+    { title: 'Expense 2', description: '', cost: '200', id: 2 },
+    { title: 'Expense 3', description: 'example description', cost: '1200',  id: 3 }
+]);
+
     return ( 
+        
         <div className="expenses">
             <h2>Expenses</h2>
-            <Link to="/add-expense">
-                <button>Add Expenses</button>
-            </Link>
+            <div className="expenses-display">
+                <Link to="/add-expense">
+                    <button>Add Expenses</button>
+                </Link>
+                {
+                    //Outputs list of expenses
+                }
+                {expenses.map((expense) => (
+                    <div className="expense-preview" key={expense.id}>
+                        <button>
+                        <h3>{expense.title}</h3>
+                        <p>{expense.description}</p>
+                        <h4>{expense.cost}$</h4>
+                        </button>  
+                    </div>
+                ))}
+            </div>
         </div>
      );
 }
--- src/Users.js
+++ src/Users.js
@@ -1,13 +1,33 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Users = () => {
+//Array of sample users
+const [users, setUsers] = useState([
+    { name: 'Name 1', balance: '0', id: 1 },
+    { name: 'Name 2', balance: '20', id: 2 },
+    { name: 'Name 3', balance: '-40', id: 3 }
+]);
 
     return ( 
         <div className="users">
             <h2>Users</h2>
-            <Link to="/add-user">
-                <button>Add User</button>
-            </Link>
+            <div className="users-display">
+                <Link to="/add-user">
+                    <button>Add User</button>
+                </Link>
+                {
+                    //Outputs list of users
+                }
+                {users.map((user) => (
+                    <div className="user-preview" key={user.id}>
+                        <button>
+                        <h3>{user.name}</h3>
+                        <p>{user.balance}</p>
+                        </button>  
+                    </div>
+                ))}
+            </div>
         </div>
      );
 }
