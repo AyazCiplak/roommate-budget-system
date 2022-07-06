7db649a5009f9dd3428e88969879e1d2e584d4be
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-07-06 14:34:34 -0400

    Data fetch error messages added for expense class

--- src/Expenses.js
+++ src/Expenses.js
@@ -8,15 +8,24 @@ const [expenses, setExpenses] = useState(null);
 
 //Loading message for user data fetch
 const [isPending, setIsPending] = useState(true);
+const [error, setError] = useState(null);
 
 useEffect(() => {
     fetch('http://localhost:8000/expenses')
     .then(res => {
+        if(!res.ok) {
+            throw Error('Error: Could not fetch data.');
+        }
         return res.json();
     })
     .then(data => {
         setExpenses(data);
         setIsPending(false);
+        setError(null);
+    })
+    .catch(err => {
+        setIsPending(false);
+        setError(err.message);
     })
 }, [])
 
@@ -28,6 +37,7 @@ useEffect(() => {
                 <Link to="/add-expense">
                     <button>Add Expenses</button>
                 </Link>
+                { error && <div>{ error }</div>}
                 { isPending && <div>Loading...</div> }
                 { expenses && <ExpenseList expenses={expenses}/> }
             </div>
