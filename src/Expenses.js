b89bdd4f1801bdd5d7d7e039dedec97e6bd6be53
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-07-06 14:46:57 -0400

    Extracted data fetch as custom hook

--- src/Expenses.js
+++ src/Expenses.js
@@ -1,33 +1,11 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import ExpenseList from './ExpenseList';
+import useFetchData from './useFetchData';
 
 const Expenses = () => {
+        const {data: expenses, isPending, error } = useFetchData('http://localhost:8000/expenses')
 
-const [expenses, setExpenses] = useState(null);
-
-//Loading message for user data fetch
-const [isPending, setIsPending] = useState(true);
-const [error, setError] = useState(null);
-
-useEffect(() => {
-    fetch('http://localhost:8000/expenses')
-    .then(res => {
-        if(!res.ok) {
-            throw Error('Error: Could not fetch data.');
-        }
-        return res.json();
-    })
-    .then(data => {
-        setExpenses(data);
-        setIsPending(false);
-        setError(null);
-    })
-    .catch(err => {
-        setIsPending(false);
-        setError(err.message);
-    })
-}, [])
 
     return ( 
         
